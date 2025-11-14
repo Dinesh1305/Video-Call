605f7ee82f66ef86a12115306c39d02250f69854
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-11-14 22:00:12 +0530

    login and logout method was added in UserService class

--- src/main/java/com/demo/user/UserService.java
+++ src/main/java/com/demo/user/UserService.java
@@ -8,13 +8,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-	private static final List<User>list=new ArrayList<>();
-
-	
-	
+	private static final List<User>list=new ArrayList<>();	
 	public void register(User user)	{
 		
 		user.setStatus("online");
 		list.add(user);;
+	}
+	
+	
+	public User login(User user)
+	{
+		for(int j=0;j<list.size();j++)
+		{
+			if(list.get(j).getUsername().equals(user.getUsername()) && list.get(j).getPassword().equals(user.getPassword()))
+			{
+				user.setStatus("online");
+				return user;
+			}
+		}
+		
+		return null;
+	}
+	
+	public void logout(String email)
+	{
+		for(int j=0;j<list.size();j++)
+		{
+			if(list.get(j).getUsername().equals(email))
+			{
+				list.get(j).setStatus("offline");
+				//return user;
+			}
+		}
 	}
 }
