d1e98027d7386ac414ecacc467d1f5c7a716e58b
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-11-15 07:23:47 +0530

    UserController wass created

---
+++ src/main/java/com/demo/user/UserController.java
@@ -0,0 +1,36 @@
+package com.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="*")
+public class UserController {
+
+	
+	@Autowired
+	UserService service;
+	
+	
+	
+	@PostMapping("login")
+	public User login(User user){
+		
+		return service.login(user);
+	}
+	
+	@PostMapping("logout")
+	public void logout(String email)
+	{
+		service.logout(email);
+	}
+	
+	@PostMapping("register")
+	public void register(User user)
+	{
+		 service.register(user);
+	}
+	
+}
